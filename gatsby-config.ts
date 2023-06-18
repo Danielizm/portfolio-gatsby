47c25957f5f6f1092c3e9bd251cb5618bb69a8ff
Daniel Su <foeverdan@gmail.com>
2023-06-18 14:30:00 +1000

    add scroll reveal

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,7 +20,9 @@ const config: GatsbyConfig = {
       "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
       "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
-  }, "gatsby-plugin-image", 
+  }, 
+    "gatsby-plugin-scroll-reveal",
+    "gatsby-plugin-image", 
     "gatsby-plugin-sharp", 
     "gatsby-transformer-sharp",
     "gatsby-plugin-transition-link", 
